1b708fed56dfe1f043beea72dc102170cf711529
huyite <huyite.vn@gmail.com>
2016-10-14 22:05:43 +0700

    edit main

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,8 +3,8 @@ public class MainClass {
 
 	public static void main(String[] args) throws Exception {
 		//new Model1("data/input3.txt", "data/output3_M1.txt");
-		Model2 m=new Model2("data/input4.txt", "data/output4.txt");
-		m.prinfile("data/var.txt");
+		Model2 m=new Model2("data/input3.txt", "data/output3_M2.txt");
+		//m.prinfile("data/var.txt");
 
 		
 	}
